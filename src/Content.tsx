a329b2fd09a6602eb7e4064718fe2d02b9b21b31
mfuatnuroglu <mfuatnuroglu@gmail.com>
2022-10-05 08:40:43 +0300

    app-name

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,32 +1,26 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import appInfo from './app-info';
-import routes from './app-routes';
-import { SideNavInnerToolbar as SideNavBarLayout } from './layouts';
-import { Footer } from './components';
+import { Routes, Route, Navigate } from "react-router-dom";
+import appInfo from "./app-info";
+import routes from "./app-routes";
+import { SideNavInnerToolbar as SideNavBarLayout } from "./layouts";
+import { Footer } from "./components";
+import { useTranslation } from "react-i18next";
 
 export default function Content() {
+  const { t } = useTranslation();
   return (
-    <SideNavBarLayout title={appInfo.title}>
+    <SideNavBarLayout title={t(appInfo.title)}>
       <Routes>
         {routes.map(({ path, element }) => (
-          <Route
-            key={path}
-            path={path}
-            element={element}
-          />
+          <Route key={path} path={path} element={element} />
         ))}
-        <Route
-          path='*'
-          element={<Navigate to='/home' />}
-        />
+        <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
       <Footer>
         Copyright © 2011-{new Date().getFullYear()} {appInfo.title} Inc.
         <br />
-        All trademarks or registered trademarks are property of their
-        respective owners.
+        All trademarks or registered trademarks are property of their respective
+        owners.
       </Footer>
     </SideNavBarLayout>
   );
 }
-
